f558e1df0babb564deb86b4c5f6933a99e3d6a0d
N-Nilashan <informal.nimesh@gmail.com>
2026-05-17 16:58:17 +0530

    initial commit

---
+++ src/main/java/com/restaurant/Restaurant_Table_Reservation/RestaurantTableReservationApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.Restaurant_Table_Reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantTableReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantTableReservationApplication.class, args);
+	}
+
+}
