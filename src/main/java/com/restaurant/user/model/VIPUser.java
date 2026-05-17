3654a5b1c0e0939e4111c50e6ba44fcb12b5f338
nilashan-sliit <it25104131@my.sliit.lk>
2026-05-17 17:25:39 +0530

    Created user models and added updates

--- src/main/java/com/restaurant/user/model/VIPUser.java
+++ src/main/java/com/restaurant/user/model/VIPUser.java
@@ -1,4 +1,15 @@
 package com.restaurant.user.model;
 
-public class VIPUser {
+public class VIPUser extends User {
+
+    public VIPUser(String userId, String username, String password,
+                   String email, String phone) {
+        super(userId, username, password, email, phone, "VIP",
+                java.time.LocalDate.now().toString());
+    }
+
+    @Override
+    public String getUserType() {
+        return "VIP User (with discount)";
+    }
 }
